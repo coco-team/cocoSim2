344c29f6d724d0b0ce2a6719fb09b0e699d56d7a
cdambrev <christelledambreville@hotmail.fr>
2017-07-19 16:51:34 +0200

    first draft of the sinks blocks' struct

---
+++ src/ir/blocks/sinks_struct.m
@@ -0,0 +1,11 @@
+function [ S ] = sinks_struct( file_name, block_type )
+
+% Non supporté : 'Display',
+%    'Stop Simulation', 'ToFile'
+
+% Outport, Scope, Terminator, ToWorkspace : rien d'intéressant ?
+
+S = struct();
+
+end
+
