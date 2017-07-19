5687db954079ca6d821cad7ec054b52ce4cca4c1
cdambrev <christelledambreville@hotmail.fr>
2017-07-19 16:51:11 +0200

    first draft of the signal attributes blocks' struct

---
+++ src/ir/blocks/signal_attributes_struct.m
@@ -0,0 +1,12 @@
+function [ S ] = signal_attributes_struct( file_name, block_type )
+
+%Non supporté : 'BusToVector',
+%    'DataTypeDuplicate', 'InitialCondition', 'Probe', 'RateTransition',
+%    'SampleTimeMath', 'Width'
+
+%DataTypeConversion, SignalSpecification, SignalConversion : rien d'intéressant ?
+
+S = struct();
+
+end
+
