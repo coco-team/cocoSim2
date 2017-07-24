f601822e57c98f2e4ea938cbffe2dcbe0f0714b8
cdambrev <christelledambreville@hotmail.fr>
2017-07-24 19:31:20 +0200

    removal of the useless files

--- src/ir/blocks/model_verification_struct.m
+++
@@ -1,8 +0,0 @@
-function [ S ] = model_verification_struct( file_name, block_type )
-
-% Non supporté : 'Assertion'
-
-S = struct();
-
-end
-
--- src/ir/blocks/model_wide_utilities_struct.m
+++
@@ -1,8 +0,0 @@
-function [ S ] = model_wide_utilities_struct( file_name, block_type )
-
-% pas de non masked subsystem
-
-S = struct();
-
-end
-
