86eb796534e6eddc4c02e0b231f4ae4fcc45e393
cdambrev <christelledambreville@hotmail.fr>
2017-07-24 19:31:57 +0200

    Common struct finished

--- src/ir/blocks/common_struct.m
+++ src/ir/blocks/common_struct.m
@@ -7,18 +7,27 @@ S.BlockType = get_param(file_name, 'BlockType');
 ports = get_param(file_name, 'PortConnectivity');
 pre = {};
 post = {};
-for i=1:numel(ports)
-    block = get(ports(i).SrcBlock);
-    if (~isempty(block))
-        pre(numel(pre) + 1) = {block.Name};
-    else
-        block = get(ports(i).DstBlock);
-        post(numel(post) + 1) = {block.Name};
+if ~isempty(ports)
+    try
+        for i=1:numel(ports)
+            block = get(ports(i).SrcBlock);
+            if (~isempty(block))
+                pre(numel(pre) + 1) = {block.Name};
+            else
+                block = get(ports(i).DstBlock);
+                post(numel(post) + 1) = {block.Name};
+            end
+        end
+    catch
+        warning('Some ports are not linked.');
     end
 end
 
 S.pre = pre;
 S.post = post;
+
+CompiledSampleTime = get_param(file_name, 'CompiledSampleTime');
+S.CompiledSampleTime = CompiledSampleTime(1);
 
 end
 
