fa064024789975932fc204fa54a3470328138663
Bourbouh <hbourbou@ze32e3ti.ndc.nasa.gov>
2017-09-27 17:09:23 -0700

    fix lustre_verify

--- src/backEnd/verification/lustreVerify/lustre_verify.m
+++ src/backEnd/verification/lustreVerify/lustre_verify.m
@@ -1,24 +1,24 @@
 function [ ] = lustre_verify( model_full_path, const_files, default_Ts, trace, dfexport )
-%LUSTRE_VERIFY 
+%LUSTRE_VERIFY
 if nargin==5
-[nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts, trace, dfexport);
+    [nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts, trace, dfexport);
 elseif nargin==4
-[nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts, trace);
+    [nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts, trace);
 elseif nargin==3
-[nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts);
+    [nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files, default_Ts);
 elseif nargin==2
-[nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files);
+    [nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path, const_files);
 elseif nargin==1
-[nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path);
+    [nom_lustre_file, ~, c_code, property_node_names, ir_struct, xml_trace, is_SF]=lustre_compiler(model_full_path);
 end
 
 Utils.update_status('Verification');
 smt_file = '';
 Query_time = 0;
 
-[model_path, file_name, ~] = fileparts(model_full_path);
+
+[output_dir, file_name, ~] = fileparts(nom_lustre_file);
 [models, ~] = find_mdlrefs(file_name);
-output_dir = fullfile(model_path, strcat('lustre_files/src_', file_name));
 property_file_base_name = fullfile(output_dir, strcat(file_name, '.property'));
 
 try
@@ -79,3 +79,148 @@ end
 
 end
 
+function update_properties_gui(properties_summary, model_full_path, lus_dir)
+[~, file_name, ~] = fileparts(model_full_path);
+try
+    tgroup = evalin('base','cocosim_tgroup_handle');
+    if (tgroup.isvalid)
+        tgroup_found  = true;
+    else
+        tgroup_found  = false;
+    end
+catch
+    tgroup_found  = false;
+end
+if tgroup_found && isa(tgroup,'matlab.ui.container.TabGroup')
+    nb_pp = numel(properties_summary);
+    fields_nb = nb_pp + 4;% properties +  titles +buttons
+    space = 1 / (fields_nb + 1);
+    panel = tgroup.Children(6).Children(1);
+    if panel.isvalid && isa(panel,'matlab.ui.container.Panel')
+        panel.Children = [];
+        uicontrol(panel,'Style','text',...
+            'String','Safe properties :','HorizontalAlignment','left',...
+            'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+            'Units', 'Normalized','Position',[0.05 space*fields_nb 0.8 space]);
+        j = 1;
+        cex_indexes = [];
+        timeout_indexes = [];
+        unknown_indexes = [];
+        for i=1:nb_pp
+            answer = properties_summary(i).Answer;
+            if strcmp(answer, 'SAFE')
+                name = properties_summary(i).Name;
+                uicontrol(panel,'Style','text',...
+                    'String',name,'HorizontalAlignment','left',...
+                    'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                    'ForegroundColor', 'blue', ...
+                    'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.8 space]);
+                j = j + 1;
+            elseif strcmp(answer, 'CEX')
+                cex_indexes = [cex_indexes, i];
+            elseif strcmp(answer, 'TIMEOUT')
+                timeout_indexes = [timeout_indexes, i];
+            else
+                unknown_indexes = [unknown_indexes, i];
+            end
+        end
+        if j > 1
+            uicontrol(panel,'Style','pushbutton',...
+                'String','View generated CoCoSpec (Experimental)','HorizontalAlignment','left',...
+                'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.3 space],...
+                'Callback', @viewContractCallback)
+            j = j + 1;
+        else
+            uicontrol(panel,'Style','text',...
+                'String','No safe properties','HorizontalAlignment','left',...
+                'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                'ForegroundColor', 'blue', ...
+                'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.8 space]);
+            j = j + 1;
+        end
+        %CEX
+        if numel(cex_indexes) > 0
+            uicontrol(panel,'Style','text',...
+                'String','Unsafe properties :','HorizontalAlignment','left',...
+                'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                'Units', 'Normalized','Position',[0.05 space*(fields_nb - j) 0.8 space]);
+            j = j + 1;
+            for i=cex_indexes
+                name = properties_summary(i).Name;
+                uicontrol(panel,'Style','text',...
+                    'String',name,'HorizontalAlignment','left',...
+                    'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                    'ForegroundColor', 'red', ...
+                    'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.3 space]);
+                
+                uicontrol(panel,'Style','pushbutton',...
+                    'String','View Counter example','HorizontalAlignment','left',...
+                    'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                    'Units', 'Normalized','Position',[0.5 space*(fields_nb - j+ 0.25) 0.25 space],...
+                    'Callback', {@viewCEX,name})
+                
+                j = j + 1;
+            end
+        end
+        
+        if numel(timeout_indexes) > 0
+            uicontrol(panel,'Style','text',...
+                'String','TIMEOUT properties :','HorizontalAlignment','left',...
+                'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                'Units', 'Normalized','Position',[0.05 space*(fields_nb - j) 0.8 space]);
+            j = j + 1;
+            for i=timeout_indexes
+                name = properties_summary(i).Name;
+                uicontrol(panel,'Style','text',...
+                    'String',name,'HorizontalAlignment','left',...
+                    'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                    'ForegroundColor', 'red', ...
+                    'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.8 space]);
+                j = j + 1;
+            end
+        end
+        
+        if numel(unknown_indexes) > 0
+            uicontrol(panel,'Style','text',...
+                'String','UNKNOWN properties :','HorizontalAlignment','left',...
+                'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                'Units', 'Normalized','Position',[0.05 space*(fields_nb - j) 0.8 space]);
+            j = j + 1;
+            for i=unknown_indexes
+                name = properties_summary(i).Name;
+                uicontrol(panel,'Style','text',...
+                    'String',name,'HorizontalAlignment','left',...
+                    'FontUnits', 'Normalized', 'FontSize',space, 'FontWeight', 'bold', ...
+                    'ForegroundColor', 'red', ...
+                    'Units', 'Normalized','Position',[0.15 space*(fields_nb - j) 0.8 space]);
+                j = j + 1;
+            end
+        end
+    end
+end
+    function viewCEX(src, callbackInfo, prop_name)
+        html_output = fullfile(lus_dir, strcat(prop_name,'.html'));
+        if exist(html_output, 'file')
+            open(html_output);
+        else
+            errordlg('FAILURE to parse the counter example');
+        end
+    end
+    function viewContractCallback(src, callbackInfo)
+        emf_name = [file_name '_EMF'];
+        try
+            EMF = evalin('base', emf_name);
+        catch ME
+            display_msg(ME.getReport(),Constants.DEBUG,'viewContract','');
+            msg = sprintf('No CoCoSpec Contract for %s \n Verify the model with Zustre', file_name);
+            warndlg(msg,'CoCoSim: Warning');
+            return;
+        end
+        try
+            Output_url = view_cocospec(model_full_path, char(EMF));
+        catch ME
+            display_msg(ME.getReport(),Constants.DEBUG,'viewContract','');
+        end
+    end
+end
