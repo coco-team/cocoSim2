af2d8f0cefeacc19fcc52995a4afa44eb1fde9ee
ploc <ploc@garoche.net>
2017-11-25 02:57:29 +0100

    [pp] renaming numerical name is now prefixed with xx.      links to libraries are broken to enable component processing

--- src/frontEnd/pp/blocks/rename_numerical_prefix.m
+++ src/frontEnd/pp/blocks/rename_numerical_prefix.m
@@ -1,8 +1,9 @@
 function [] = rename_numerical_prefix(model)
-% RENAME_NUMERICAL_PREFIX Replaces all blocks which name starts with a number by a prefix __
+% RENAME_NUMERICAL_PREFIX Replaces all blocks which name starts with a
+% number by a prefix xx
 
 % Processing all blocks
-block_list = find_system(model,'Regexp','on','Name','^[0-9]')
+block_list = find_system(model,'Regexp','on','Name','^[0-9]');
 if not(isempty(block_list))
     disp('Processing numerical-prefix blocks...')
     display_msg('Processing numerical-prefix blocks...', Constants.INFO, ...
@@ -10,7 +11,7 @@ if not(isempty(block_list))
     for i=1:length(block_list)
         display_msg(block_list{i}, Constants.INFO, 'rename_numerical_prefix', '');
         name = get_param(block_list{i},'Name');
-        set_param(block_list{1},'Name',['__' name]);
+        set_param(block_list{i},'Name',['xx' name]);
     end
     display_msg('Done\n\n', Constants.INFO, 'rename_numerical_prefix', ''); 
 end
