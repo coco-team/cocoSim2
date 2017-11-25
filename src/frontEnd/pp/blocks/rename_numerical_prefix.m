d3a7504386720accbfd85d852e4882b23122d525
ploc <ploc@garoche.net>
2017-11-25 01:44:22 +0100

    [pp] solve bug with block starting with numbers

---
+++ src/frontEnd/pp/blocks/rename_numerical_prefix.m
@@ -0,0 +1,18 @@
+function [] = rename_numerical_prefix(model)
+% RENAME_NUMERICAL_PREFIX Replaces all blocks which name starts with a number by a prefix __
+
+% Processing all blocks
+block_list = find_system(model,'Regexp','on','Name','^[0-9]')
+if not(isempty(block_list))
+    disp('Processing numerical-prefix blocks...')
+    display_msg('Processing numerical-prefix blocks...', Constants.INFO, ...
+        'rename_numerical_prefix', '');
+    for i=1:length(block_list)
+        display_msg(block_list{i}, Constants.INFO, 'rename_numerical_prefix', '');
+        name = get_param(block_list{i},'Name');
+        set_param(block_list{1},'Name',['__' name]);
+    end
+    display_msg('Done\n\n', Constants.INFO, 'rename_numerical_prefix', ''); 
+end
+end
+
