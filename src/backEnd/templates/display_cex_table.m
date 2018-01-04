4212d2739d0f13763892b8b7f4d5f948ee1ecdf7
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-01-04 14:29:31 -0600

    Added row names to the ui table

--- src/backEnd/templates/display_cex_table.m
+++ src/backEnd/templates/display_cex_table.m
@@ -2,20 +2,20 @@ load('[(matFile)]');
 values = {Inputs_[(propertyName)] , Outputs_[(propertyName)]};
 f = figure;
 t = uitable(f);
-columnNames{1} = '';
 for j= 1 : length(values{1}.time)
-    columnNames{j+1} = values{1}.time(j);
+    columnNames{j} = values{1}.time(j);
 end
 t.ColumnName = columnNames;
 t.ColumnEditable = true;
 
+rowNames = {};
 %input variables
 inputVariables = length(values{1}.signals);
 for i = 1: inputVariables
     name = strcat(values{1}.signals(i).var_name, ' (input)');
-    data{i,1}  = name;
+    rowNames{i}  = name;
     for j=1 : length(values{1}.time)
-        data{i, j+1} = values{1}.signals(i).values(j);
+        data{i, j} = values{1}.signals(i).values(j);
     end
 end
 %output variables
@@ -24,12 +24,13 @@ if [(displayOutput)]
     outputVariables = length(values{2}.signals);
     for i = 1: outputVariables
         name = strcat(values{2}.signals(i).var_name, ' (output)');
-        data{i + inputVariables,1}  = name;
+        rowNames{i + inputVariables}  = name;
         for j=1 : length(values{2}.time)
-            data{i +inputVariables, j+1} = values{2}.signals(i).values(j);
+            data{i +inputVariables, j} = values{2}.signals(i).values(j);
         end
     end
 end
 t.Data = data;
+t.RowName = rowNames;
 
 
