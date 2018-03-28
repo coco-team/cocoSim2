9ac4c3589be5b7488b852e0032d598aa583d5c77
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-03-28 17:37:59 -0500

    Added outer transitions to states

--- src/frontEnd/IR/blocks/chart_struct.m
+++ src/frontEnd/IR/blocks/chart_struct.m
@@ -20,19 +20,53 @@ function [chartStruct] = chart_struct(chartPath)
     model = stateflowRoot.find('-isa', 'Simulink.BlockDiagram', 'Name',modelName);
     % get the chart object
     chart = model.find('-isa','Stateflow.Chart', 'Path', chartPath);   
-    % get the states of the chart
-    chartStates = chart.find('-isa','Stateflow.State');
     
-    transitions = chart.find('-isa','Stateflow.Transition');
+    %get the chart path
+    chartStruct.SFCHART.origin_path = chart.Path;
+    
+    % create a map that holds all needed chart objects
+    chartObjectsMap = containers.Map('KeyType','int32','ValueType','any');
+    
+    % a convenient function to add chart objects to chartObjectsMap
+    function addObjectToMap(chartObject)
+        chartObjectsMap(chartObject.id) = chartObject;
+    end
+    
+    % get the states in the chart
+    chartStates = chart.find('-isa','Stateflow.State');
+    % add chart states to the map
+    arrayfun(@addObjectToMap, chartStates);
+    %get the transitions in the chart
+    chartTransitions = chart.find('-isa','Stateflow.Transition');
+    % add chart transitions to the map
+    arrayfun(@addObjectToMap, chartTransitions);
+       
+    sourceTransitionMap = containers.Map('KeyType','int32','ValueType','any');
+        
+    for i = 1 : length(chartTransitions)
+        if ~ isempty(chartTransitions(i).Source) 
+            id = chartTransitions(i).Source.id;
+            if isKey(sourceTransitionMap, id)
+                sourceTransitionMap(id) = [sourceTransitionMap(id) chartTransitions(i)];
+            else
+                sourceTransitionMap(id) = chartTransitions(i);
+            end
+        end
+    end
     
     % build the json struct for states
-    chartStruct.states = cell(length(chartStates),1);
+    chartStruct.SFCHART.states = cell(length(chartStates),1);
     for index = 1 : length(chartStates)
-        chartStruct.states{index} = buildStateStruct(chartStates(index));
+        % if the state is a source of some transitions
+        stateTransitions = [];
+        if isKey(sourceTransitionMap, chartStates(index).id)
+            stateTransitions = sourceTransitionMap(chartStates(index).id);
+        end
+        chartStruct.SFCHART.states{index} = buildStateStruct(chartStates(index), stateTransitions);
     end
 end
 
-function stateStruct =  buildStateStruct(state)    
+function stateStruct =  buildStateStruct(state, stateTransitions)    
     % set the state path
     stateStruct.path = state.Path;
     
@@ -44,5 +78,23 @@ function stateStruct =  buildStateStruct(state)
     stateStruct.state_actions = {};  
     for i = 1 : length(keys)
         stateStruct.state_actions.(keys(i)) = char(hashMap.get(keys(i)));
+    end    
+    
+    % set the state transitions    
+    stateStruct.outer_trans = {};
+    for i = 1 : length(stateTransitions)
+       transitionStruct.id = stateTransitions(i).id;
+       
+       transitionStruct.dest.id = stateTransitions(i).Destination.id;
+       
+       % check if the destination is a junction or a state
+       if strcmp(stateTransitions(i).Destination.Type, 'CONNECTIVE')
+           transitionStruct.dest.type = 'Junction';
+           transitionStruct.dest.name = '';
+       else
+           transitionStruct.dest.type = 'State';
+           transitionStruct.dest.name = stateTransitions(i).Destination.name;
+       end                
+       stateStruct.outer_trans = [stateStruct.outer_trans transitionStruct];
     end    
 end
