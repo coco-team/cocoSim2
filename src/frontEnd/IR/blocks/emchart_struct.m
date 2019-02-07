be05e4f7512633f0dfb02518f51ea753724cc726
Bourbouh <hamza.bourbouh@nasa.gov>
2019-02-06 16:18:06 -0800

    Update emchart_struct.m

--- src/frontEnd/IR/blocks/emchart_struct.m
+++ src/frontEnd/IR/blocks/emchart_struct.m
@@ -1,8 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This file is part of CoCoSim.
-% Copyright (C) 2018  The university of Iowa
-% Authors: Mudathir Mahgoub, Hamza Bourbouh
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [StateflowContentStruct] = emchart_struct(emchartPath)
     StateflowContentStruct = {};
     blockObject = find(slroot, '-isa', 'Stateflow.EMChart', 'Path', emchartPath);
