87a4ae593477b6a563e0426d90fd5007710e5257
Bourbouh <hamza.bourbouh@nasa.gov>
2019-03-25 20:26:33 -0700

    Update saveValidatorParameters.m

--- libs/contract/saveValidatorParameters.m
+++ libs/contract/saveValidatorParameters.m
@@ -6,27 +6,31 @@
 
 
 function saveValidatorParameters(blockPath)
-values = get_param(blockPath,'MaskValues');
-
-ContractValidatorBlock.block = blockPath;
-
-ContractValidatorBlock.assumePorts = str2num(char(values(1)));
-ContractValidatorBlock.guaranteePorts = str2num(char(values(2)));
-ContractValidatorBlock.modeBlocksPorts = str2num(char(values(3)));
-
-ContractValidatorBlock.portHandles = get_param(ContractValidatorBlock.block, 'PortHandles');
-ContractValidatorBlock.portConnectivity =get_param(ContractValidatorBlock.block, 'PortConnectivity');
-
-% store ContractValidatorBlock in ContractValidatorBlocksMap
-modelWorkspace = get_param(bdroot,'ModelWorkspace');
-if modelWorkspace.hasVariable('ContractValidatorBlocksMap')
-    ContractValidatorBlocksMap = modelWorkspace.getVariable('ContractValidatorBlocksMap');
-else
-    ContractValidatorBlocksMap = containers.Map;
-end
-
-ContractValidatorBlocksMap(ContractValidatorBlock.block) = ContractValidatorBlock;
-assignin(modelWorkspace,'ContractValidatorBlocksMap',ContractValidatorBlocksMap); 
-
+    try
+        values = get_param(blockPath,'MaskValues');
+        
+        ContractValidatorBlock.block = blockPath;
+        
+        ContractValidatorBlock.assumePorts = str2num(char(values(1)));
+        ContractValidatorBlock.guaranteePorts = str2num(char(values(2)));
+        ContractValidatorBlock.modeBlocksPorts = str2num(char(values(3)));
+        
+        ContractValidatorBlock.portHandles = get_param(ContractValidatorBlock.block, 'PortHandles');
+        ContractValidatorBlock.portConnectivity =get_param(ContractValidatorBlock.block, 'PortConnectivity');
+        
+        % store ContractValidatorBlock in ContractValidatorBlocksMap
+        modelWorkspace = get_param(bdroot,'ModelWorkspace');
+        if modelWorkspace.hasVariable('ContractValidatorBlocksMap')
+            ContractValidatorBlocksMap = modelWorkspace.getVariable('ContractValidatorBlocksMap');
+        else
+            ContractValidatorBlocksMap = containers.Map;
+        end
+        
+        ContractValidatorBlocksMap(ContractValidatorBlock.block) = ContractValidatorBlock;
+        assignin(modelWorkspace,'ContractValidatorBlocksMap',ContractValidatorBlocksMap);
+    catch me
+        % handle exception here.
+    end
+    
 end
 
