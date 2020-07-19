9fa71bb40c17198cf19a704ca11252d8c5c2a3c2
Bourbouh <hamza.bourbouh@nasa.gov>
2020-07-19 18:17:35 +0200

    Update PreferencesMenu.m

--- src/preferences/PreferencesMenu.m
+++ src/preferences/PreferencesMenu.m
@@ -213,7 +213,7 @@ classdef PreferencesMenu
             CoCoSimPreferences = callbackInfo.userdata;            
             
             % ToDo: remove the hardcoded options
-            timeoutOptions = [1 3 5 10 20 60 120];
+            timeoutOptions = [0 1 3 5 10 20 60 120];
             data = {};
             data.selectedOption = CoCoSimPreferences.verificationTimeout / 60; % seconds
             data.CoCoSimPreferences = CoCoSimPreferences;
@@ -226,8 +226,10 @@ classdef PreferencesMenu
         
         function schema = timeoutOption(callbackInfo)
             schema = sl_toggle_schema;
-            data = callbackInfo.userdata;    
-            if data.currentOption == 1
+            data = callbackInfo.userdata;  
+            if data.currentOption == 0
+                schema.label = 'No timeout';
+            elseif data.currentOption == 1
                 schema.label = '1 minute';
             else
                 schema.label = strcat(num2str(data.currentOption), ' minutes');
